f7c12b471d1d362a2295c918eab8308ab7d27529
luisnishimori <35077227+luisnishimori@users.noreply.github.com>
2020-11-09 06:37:47 -0300

    Add files via upload

---
+++ Inst-DVWA.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y upgrade
+
+logger "Installing DVWA"
+
+# install required packages
+sudo apt install apache2 mysql-server php php-mysqli php-gd libapache2-mod-php git
+
+# install DVWA
+cd ~
+git clone --recursive https://github.com/ethicalhack3r/DVWA.git
+sudo rm /var/www/html/index.html
+sudo cp -r ~/DVWA/* /var/www/html/
+cd /var/www/html
+ls -la /var/www/html/
+sudo cp config/config.inc.php.dist config/config.inc.php
+cd /var/www/html
+sudo chmod 757 /var/www/html/hackable/uploads/
+sudo chmod 646 /var/www/html/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt 
+sudo chmod 777 /var/www/html/config
+sudo systemctl restart apache2
+
+# MySQL Setup
+sudo mysql -uroot
+DROP USER 'root'@'localhost';
+CREATE USER 'dvwa'@'localhost' IDENTIFIED BY 'p@ssw0rd';
+GRANT ALL PRIVILEGES ON *.* TO 'dvwa'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+exit
