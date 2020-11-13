1b82077cfe6ac47a25bca694f437c42ab59dc0aa
luisnishimori <35077227+luisnishimori@users.noreply.github.com>
2020-11-13 16:36:28 -0300

    Update Inst-DVWA.sh

--- Inst-DVWA.sh
+++ Inst-DVWA.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-apt-get -y update
-apt-get -y upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 logger "Installing DVWA"
 
 # install required packages
-# bash cd ~
-sudo apt install apache2 mysql-server php php-mysqli php-gd libapache2-mod-php git -y
+cd ~
+sudo apt -y install apache2 mysql-server php php-mysqli php-gd libapache2-mod-php git
 
 # install DVWA
-bash cd ~
-bash git clone --recursive https://github.com/ethicalhack3r/DVWA.git
-# bash sudo rm /var/www/html/index.html
-# bash sudo cp -r ~/DVWA/* /var/www/html/
-# bash cd /var/www/html
-# bash ls -la /var/www/html/
-# bash sudo cp config/config.inc.php.dist config/config.inc.php
-# bash cd /var/www/html
-# bash sudo chmod 757 /var/www/html/hackable/uploads/
-# bash sudo chmod 646 /var/www/html/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt 
-# bash sudo chmod 777 /var/www/html/config
-# bash sudo systemctl restart apache2
+cd ~
+git clone --recursive https://github.com/ethicalhack3r/DVWA.git
+sudo rm /var/www/html/index.html
+sudo cp -r ~/DVWA/* /var/www/html/
+cd /var/www/html
+ls -la /var/www/html/
+sudo cp config/config.inc.php.dist config/config.inc.php
+cd /var/www/html
+sudo chmod 757 /var/www/html/hackable/uploads/
+sudo chmod 646 /var/www/html/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt 
+sudo chmod 777 /var/www/html/config
+sudo systemctl restart apache2
 
-# MySQL Setup
-# bash sudo mysql -uroot
-# DROP USER 'root'@'localhost';
-# CREATE USER 'dvwa'@'localhost' IDENTIFIED BY 'p@ssw0rd';
-# GRANT ALL PRIVILEGES ON *.* TO 'dvwa'@'localhost' WITH GRANT OPTION;
-# FLUSH PRIVILEGES;
-# exit
+MySQL Setup
+sudo mysql -uroot
+DROP USER 'root'@'localhost';
+CREATE USER 'dvwa'@'localhost' IDENTIFIED BY 'p@ssw0rd';
+GRANT ALL PRIVILEGES ON *.* TO 'dvwa'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+exit
